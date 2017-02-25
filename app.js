521be410aabb2c08e943dadf01ee3b7ee918ec22
Ogunlana Tunbosun <tunbosunogunlana1@gmail.com>
2017-02-25 14:14:27 +0100

    Module 1 assignment solution

---
+++ app.js
@@ -0,0 +1,27 @@
+(function (){
+  'use strict';
+
+  angular.module('lunchChecker', [])
+  .controller('lunchController', lunchController);
+  lunchController.$inject = ['$scope'];
+  function lunchController($scope){
+
+    $scope.lunchMenuFunc = function(){
+    $scope.menu = $scope.lunchMenu;
+    $scope.menu1 = $scope.menu.split(',');
+     $scope.message = reply($scope.menu1);
+      };
+
+function reply(string){
+    if(string === " "){
+      return "Please enter data first"
+    } else{
+    if(string.length <= 3){
+    return "Enjoy!"
+  }else {
+    return "Too Much!"
+  };
+  };
+}
+  }
+})();
